853160f4848a5e8779d0b4bcca060d0777662a39
liguowei <Numblgw@163.com>
2021-05-31 22:58:09 +0800

    init

---
+++ aquarium-apiserver/src/main/java/com/seaworld/aquarium/apiserver/Bootstrap.java
@@ -0,0 +1,12 @@
+package com.seaworld.aquarium.apiserver;
+
+/**
+ * @author liguowei
+ * @date 2021-05-31 22:32
+ */
+public class Bootstrap {
+
+  public static void main(String[] args) {
+
+  }
+}
